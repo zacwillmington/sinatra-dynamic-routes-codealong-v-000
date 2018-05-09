3b33712247b3a726eab05e6714558c7efb18eed5
Zac Willmington <zac.willmington@gmail.com>
2018-05-09 18:51:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ class App < Sinatra::Base
 
   get "/multiply/:num1/:num2" do
       binding.pry
-      @number1 = params[:num1]
-      @number2 = params[:num2]
-      params[:num1] + params[:num2]
+      @number1 = params[:num1].to_i
+      @number2 = params[:num2].to_i
+      @number1 + @number2
   end
 end
